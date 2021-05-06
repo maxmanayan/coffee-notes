d5674b2b7ec1fc433440837babd355702ecac1fb
Max Manayan <maximillian521@gmail.com>
2021-05-06 15:53:07 -0600

    notes backend setup with custom sql

--- app/controllers/api/notes_controller.rb
+++ app/controllers/api/notes_controller.rb
@@ -1,2 +1,53 @@
 class Api::NotesController < ApplicationController
+  before_action :get_note, only: [:show, :update, :destroy]
+
+  def index 
+    render json: Note.all
+  end
+
+  def show
+    render json: @note
+  end
+
+  def create
+    new_note = Note.new(note_params)
+
+    if (new_note.save)
+      render json: new_note
+    else
+      render json: {errors: new_note.errors}
+    end
+  end
+
+  def update
+    if @note.update(note_params)
+      render json: @note
+    else
+      render json: {errors: new_note.errors}
+    end
+  end
+
+  def destroy
+    render json: @note.destroy
+  end
+
+  def get_todo_notes
+    todo_notes = Note.get_todo_notes
+    render json: todo_notes
+  end
+
+  def get_completed_notes
+    completed_notes = Note.get_completed_notes
+    render json: completed_notes
+  end
+
+  private
+
+  def get_note
+    @note = Note.find(params[:id])
+  end
+
+  def note_params
+    params.require(:note).permit(:title, :body, :text)
+  end
 end
--- app/models/note.rb
+++ app/models/note.rb
@@ -1,2 +1,14 @@
 class Note < ApplicationRecord
+
+  def self.get_todo_notes
+    select("*")
+    .from("notes")
+    .where("completed = false")
+  end
+
+  def self.get_completed_notes
+    select("*")
+    .from("notes")
+    .where("completed = true")
+  end
 end
