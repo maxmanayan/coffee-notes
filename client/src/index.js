aaf56351b23b1daa412887cbcbfca68d6d2560f7
Max Manayan <maximillian521@gmail.com>
2021-05-21 14:45:05 -0500

    added authprovider to index

--- client/src/index.js
+++ client/src/index.js
@@ -5,11 +5,14 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import "bootstrap/dist/css/bootstrap.css";
 import { BrowserRouter } from 'react-router-dom';
+import AuthProvider from "./providers/AuthProvider"
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <AuthProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </AuthProvider>,
   document.getElementById('root')
 );
 
