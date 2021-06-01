050adb47756732cd7efb081c14005ca07720c824
Max Manayan <maximillian521@gmail.com>
2021-06-01 12:01:48 -0500

    styled todo items

--- client/src/components/TodoList.js
+++ client/src/components/TodoList.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 
 const TodoList = () => {
+  const [items, setItems] = useState(null)
+
+  useEffect(()=>{
+    getItems()
+  },[])
+
+  const getItems = async () => {
+    try {
+      let res = await axios.get('/api/items')
+      setItems(res.data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const renderItems = () => {
+    return items.map(item => {
+      return(
+        <p className='todo-item'>{item.todo}</p>
+      )
+    })
+  }
+
   return(
     <>
       <h1>TodoList</h1>
+      <div className='todo-list'>
+        {items && renderItems()}
+      </div>
     </>
   )
 }
