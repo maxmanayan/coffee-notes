8a36ad81497823a32f949f799be7049faace7792
Max Manayan <maximillian521@gmail.com>
2021-06-01 11:32:18 -0500

    todo item model and controller setup

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_19_212121) do
+ActiveRecord::Schema.define(version: 2021_06_01_163032) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "items", force: :cascade do |t|
+    t.string "todo"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "notes", force: :cascade do |t|
     t.string "title"
