46b559f93edfdd1773e0446a47a8a76d0e87af30
Max Manayan <maximillian521@gmail.com>
2021-06-02 17:19:04 -0500

    todo list functional

--- client/src/components/TodoList.js
+++ client/src/components/TodoList.js
@@ -14,25 +14,65 @@ const TodoList = (props) => {
     console.log('note', note)
     try {
       let res = await axios.get(`/api/notes/${note.id}/items`)
-      console.log('in getItems', res.data)
       setItems(res.data)
     } catch (error) {
       console.log(error)
     }
   }
 
-  const renderItems = () => {
+  const editComplete = async (item) => {
+    try {
+      if (!item.completed) {
+        await axios.put(`/api/notes/${note.id}/items/${item.id}`, {
+          content: note.content,
+          completed: true
+        })
+        getItems()
+      }
+      if (item.completed) {
+        console.log('item', item)
+        await axios.put(`/api/notes/${note.id}/items/${item.id}`, {
+          content: note.content,
+          completed: false
+        })
+        getItems()
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const renderTodoItems = () => {
     return items.map(item => {
-      return(
-        <div className='todo-item-container'>
-          <div>
-            <Icon.CheckSquare className='todo-check' size={25} />
+      if (!item.completed) {
+        return(
+          <div className='todo-item-container'>
+            <div>
+              <Icon.CheckSquare className='todo-check' onClick={() => editComplete(item)} size={25} />
+            </div>
+            <div>
+              <p className='todo-item'>{item.content}</p>
+            </div>
           </div>
-          <div>
-            <p className='todo-item'>{item.content}</p>
+        )
+      }
+    })
+  }
+
+  const renderCompletedItems = () => {
+    return items.map(item => {
+      if (item.completed) {
+        return(
+          <div className='todo-item-container'>
+            <div>
+              <Icon.CheckSquare className='todo-check-completed' onClick={() => editComplete(item)} size={25} />
+            </div>
+            <div>
+              <p className='todo-item'>{item.content}</p>
+            </div>
           </div>
-        </div>
-      )
+        )
+      }
     })
   }
 
@@ -41,7 +81,12 @@ const TodoList = (props) => {
       <h1>{note.title}</h1>
       <h5>{note.body}</h5>
       <div className='todo-list'>
-        {items && renderItems()}
+        <h6>Todo</h6>
+        {items && renderTodoItems()}
+      </div>
+      <div className='todo-list-completed'>
+        <h6>Completed</h6>
+        {items && renderCompletedItems()}
       </div>
     </>
   )
