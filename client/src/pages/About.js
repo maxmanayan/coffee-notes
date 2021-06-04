2773f486d57f03e96b6d04a6e8ebbb1c1b9b3d70
Max Manayan <maximillian521@gmail.com>
2021-06-04 17:24:46 -0500

    about nav landing styled

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
+import * as Icon from 'react-bootstrap-icons';
 import AboutComingSoon from '../components/AboutComingSoon';
 import AboutCreator from '../components/AboutCreator';
 import AboutWebsite from '../components/AboutWebsite';
@@ -7,11 +8,20 @@ import AboutWebsite from '../components/AboutWebsite';
 const About = () => {
   return(
     <div className='about'>
-      <h1>About</h1>
-      <div>
-        <Button>Coffee Notes</Button>
-        <Button>Max Manayan</Button>
-        <Button>Future Features</Button>
+      <h1 className='about-header'>About</h1>
+      <div className='about-button-container'>
+        <div className='about-icon-website'>
+          <Icon.TvFill color='#090804' size={200}/>
+          <h3 className='about-icon-text'>Coffee Notes</h3>
+        </div>
+        <div className='about-icon-creator'>
+          <Icon.PersonFill color='#090804' size={200}/>
+          <h3 className='about-icon-text'>The Developer</h3>
+        </div>
+        <div className='about-icon-coming-soon'>
+          <Icon.ForwardFill color='#090804' size={200}/>
+          <h3 className='about-icon-text'>Future Features</h3>
+        </div>
       </div>
       <div>
         <AboutWebsite />
