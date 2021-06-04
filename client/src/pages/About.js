625c153d3a6186949dbbaf6e964c365091ce6805
Max Manayan <maximillian521@gmail.com>
2021-06-04 15:55:12 -0500

    about page backgrounds setup

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
 import AboutComingSoon from '../components/AboutComingSoon';
 import AboutCreator from '../components/AboutCreator';
 import AboutWebsite from '../components/AboutWebsite';
@@ -7,6 +8,11 @@ const About = () => {
   return(
     <div className='about'>
       <h1>About</h1>
+      <div>
+        <Button>Coffee Notes</Button>
+        <Button>Max Manayan</Button>
+        <Button>Future Features</Button>
+      </div>
       <div>
         <AboutWebsite />
         <AboutCreator />
