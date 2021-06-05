e36bd18a5f45ca48be05619e0c90dd1d983bb834
Max Manayan <maximillian521@gmail.com>
2021-06-04 19:36:23 -0500

    about website component complete

--- client/src/components/AboutWebsite.js
+++ client/src/components/AboutWebsite.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { Image } from 'react-bootstrap';
+import dash from '../images/coffee-notes-dash.jpg'
 
 const AboutWebsite = () => {
   return(
     <div id='about-website' className='about-website'>
-      <h1>AboutWebsite</h1>
+      <h1 className='about-website-header'>About Coffee Notes</h1>
+      <Image src={dash} style={{height: '50vh', borderRadius: '5px'}} />
+      <div className='about-website-text-container'>
+        <h3 className='about-website-text'>The Best Parts of a Cafe... To Go!</h3>
+        <p className='about-website-text'>
+          Coffee Notes is an app built to improve task organization.
+          Inspired by the greatest drink on the face of the Earth,
+          the design of this app is meant to establish a calm, focused mood
+          in its users. Those who use Coffee Notes are encouraged to take advantage
+          of the drag and drop notes and built-in checklist. 
+        </p>
+      </div>
     </div>
   )
 }
