07a8f8e43927a0dc61d4b52276abdc9db62b2f0d
Max Manayan <maximillian521@gmail.com>
2021-06-08 23:10:53 -0500

    need to add user ownership of notes

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@
 class User < ActiveRecord::Base
   extend Devise::Models
 
+  # has_many :items, dependent: :destroy
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
