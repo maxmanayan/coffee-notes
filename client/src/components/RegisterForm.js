a06745dbf05b8eaf059410f177932b3fb278d4ca
Max Manayan <maximillian521@gmail.com>
2021-06-14 16:30:13 -0500

    register error handling complete

--- client/src/components/RegisterForm.js
+++ client/src/components/RegisterForm.js
@@ -11,6 +11,9 @@ const RegisterForm = () => {
   const [noEmail, setNoEmail] = useState(false)
   const [noPassword, setNoPassword] = useState(false)
   const [noPasswordConfirmation, setNoPasswordConfirmation] = useState(false)
+  const [invalidPasswordLength, setInvalidPasswordLength] = useState(false)
+  const [passwordAndConfirmationNoMatch, setPasswordAndConfirmationNoMatch] = useState(false)
+  const [invalidEmail, setInvalidEmail] = useState(false)
 
   const [account, setAccount] = useState({
     name: '',
@@ -43,6 +46,25 @@ const RegisterForm = () => {
     } else {
       setNoPasswordConfirmation(false)
     }
+    if (account.password.split('').length < 6) {
+      setInvalidPasswordLength(true)
+    } else {
+      setInvalidPasswordLength(false)
+    }
+    if (account.password !== account.passwordConfirmation) {
+      setPasswordAndConfirmationNoMatch(true)
+      return
+    } else {
+      setPasswordAndConfirmationNoMatch(false)
+    }
+    
+    let emailFormat = new RegExp(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)
+    if (!emailFormat.test(account.email)) {
+      setInvalidEmail(true)
+      return
+    } else {
+      setInvalidEmail(false)
+    }
 
     console.log('account', account)
     console.log('account.name', account.name)
@@ -62,14 +84,17 @@ const RegisterForm = () => {
           <Form.Control placeholder='Email' id='email' value={account.email} onChange={(e) => setAccount({...account, email: e.target.value})} />
         </Form.Group>
         {noEmail && <p className='login-register-error-text'>*Email required to register</p>}
+        {invalidEmail && <p className='login-register-error-text'>*Invalid email address</p>}
         <Form.Group className='login-register-form-group'>
           <Form.Control placeholder='Password' id='password' value={account.password} onChange={(e) => setAccount({...account, password: e.target.value})} />
         </Form.Group>
         {noPassword && <p className='login-register-error-text'>*Password required to register</p>}
+        {invalidPasswordLength && <p className='login-register-error-text'>*Password must be at least 6 characters</p>}
         <Form.Group className='login-register-form-group'>
           <Form.Control placeholder='Confirm Password' id='passwordConfirmation' value={account.passwordConfirmation} onChange={(e) => setAccount({...account, passwordConfirmation: e.target.value})} />
         </Form.Group>
         {noPasswordConfirmation && <p className='login-register-error-text'>*Password must be confirmed</p>}
+        {passwordAndConfirmationNoMatch && <p className='login-register-error-text'>*Password and confirmation do not match</p>}
         <Button style={{marginTop: '1em', background: '#090804', border: 'none'}} type='submit'>SUBMIT</Button>
       </Form>
     </>
