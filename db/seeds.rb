cd682e2b0e35d5377d472d8bf5a41b94b5692d23
Max Manayan <maximillian521@gmail.com>
2021-06-17 13:29:11 -0500

    cleaned seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,29 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-
-# require "faker"
-# Note.destroy_all
-# Item.destroy_all
-
-# 5.times do 
-#   note = Note.create(
-#     title: Faker::Movies::HarryPotter.character,
-#     body: Faker::Movies::HarryPotter.quote,
-#     completed: false
-#   )
-
-#   2.times do
-#     note.items.create(
-#       content: Faker::Movies::HarryPotter.quote,
-#       completed: false
-#     )
-#   end
-# end
-
-
-
-
-# puts "Seeded #{Note.all.size} new notes"
-# puts "Seeded #{Item.all.size} new items"
