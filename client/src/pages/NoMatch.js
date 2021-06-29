f8b8a05008fb4916034363a2c8dab95470751364
Max Manayan <maximillian521@gmail.com>
2021-06-29 15:37:32 -0500

    prettier clean profile and no match

--- client/src/pages/NoMatch.js
+++ client/src/pages/NoMatch.js
@@ -1,17 +1,19 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const NoMatch = () => {
-  return(
-    <div className='no-match'>
-      <div className='no-match-text-container'>
-        <h1 className='no-match-text'>The Page You Are Looking For Doesn't Exist!</h1>
-        <Link className='no-match-link' to='/home'>
+  return (
+    <div className="no-match">
+      <div className="no-match-text-container">
+        <h1 className="no-match-text">
+          The Page You Are Looking For Doesn't Exist!
+        </h1>
+        <Link className="no-match-link" to="/home">
           <h1> Return Home</h1>
         </Link>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default NoMatch
+export default NoMatch;
--- client/src/pages/UserProfile.js
+++ client/src/pages/UserProfile.js
@@ -1,24 +1,20 @@
-import React, { useContext } from 'react';
-import { AuthContext } from '../providers/AuthProvider';
+import React, { useContext } from "react";
+import { AuthContext } from "../providers/AuthProvider";
 
 const UserProfile = () => {
-  const { user } = useContext(AuthContext)
+  const { user } = useContext(AuthContext);
 
-  return(
-    <div className='user-profile'>
-      <h1 className='user-profile-header'>User Profile</h1>
-      {user &&
+  return (
+    <div className="user-profile">
+      <h1 className="user-profile-header">User Profile</h1>
+      {user && (
         <div>
-          <h3>
-            Name: {user.name}
-          </h3>
-          <h3>
-            Email: {user.email}
-          </h3>
+          <h3>Name: {user.name}</h3>
+          <h3>Email: {user.email}</h3>
         </div>
-      }
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default UserProfile
+export default UserProfile;
