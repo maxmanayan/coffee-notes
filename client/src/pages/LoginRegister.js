623c16582e93cfc7e3fda406f9c7809f666cb535
Max Manayan <maximillian521@gmail.com>
2021-06-29 17:12:15 -0500

    login-register user alt setup

--- client/src/pages/LoginRegister.js
+++ client/src/pages/LoginRegister.js
@@ -1,9 +1,11 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { Button, Col, Row } from "react-bootstrap";
 import LoginForm from "../components/LoginForm";
 import RegisterForm from "../components/RegisterForm";
+import { AuthContext } from "../providers/AuthProvider";
 
 const LoginRegister = () => {
+  const { user } = useContext(AuthContext);
   const [showRegisterForm, setShowRegisterForm] = useState(false);
 
   return (
@@ -14,48 +16,86 @@ const LoginRegister = () => {
         </Col>
       </Row>
 
-      <Row
-        style={{
-          paddingTop: "1em",
-          paddingBottom: "1em",
-          marginLeft: "1vw",
-          marginRight: "1vw",
-        }}
-      >
-        <Col xs={12} sm={12} md={{ span: 5, offset: 6 }}>
-          <Button
+      {!user && (
+        <>
+          <Row
             style={{
-              marginRight: "1em",
-              padding: "1em 3em",
-              background: "#09080410",
-              border: "none",
-              fontWeight: "bold",
+              paddingTop: "1em",
+              paddingBottom: "1em",
+              marginLeft: "1vw",
+              marginRight: "1vw",
             }}
-            onClick={() => setShowRegisterForm(false)}
           >
-            LOGIN
-          </Button>
-          <Button
-            style={{
-              marginRight: "1em",
-              padding: "1em 3em",
-              background: "#09080410",
-              border: "none",
-              fontWeight: "bold",
-            }}
-            onClick={() => setShowRegisterForm(true)}
-          >
-            SIGN UP
-          </Button>
-        </Col>
-      </Row>
+            <Col xs={12} sm={12} md={{ span: 5, offset: 6 }}>
+              <Button
+                style={{
+                  marginRight: "1em",
+                  padding: "1em 3em",
+                  background: "#09080410",
+                  border: "none",
+                  fontWeight: "bold",
+                }}
+                onClick={() => setShowRegisterForm(false)}
+              >
+                LOGIN
+              </Button>
+              <Button
+                style={{
+                  marginRight: "1em",
+                  padding: "1em 3em",
+                  background: "#09080410",
+                  border: "none",
+                  fontWeight: "bold",
+                }}
+                onClick={() => setShowRegisterForm(true)}
+              >
+                SIGN UP
+              </Button>
+            </Col>
+          </Row>
 
-      <Row style={{ marginLeft: "1vw", marginRight: "1vw" }}>
-        <Col xs={12} sm={12} md={{ span: 5, offset: 6 }}>
-          {!showRegisterForm && <LoginForm />}
-          {showRegisterForm && <RegisterForm />}
-        </Col>
-      </Row>
+          <Row style={{ marginLeft: "1vw", marginRight: "1vw" }}>
+            <Col xs={12} sm={12} md={{ span: 5, offset: 6 }}>
+              {!showRegisterForm && <LoginForm />}
+              {showRegisterForm && <RegisterForm />}
+            </Col>
+          </Row>
+        </>
+      )}
+
+      {user && console.log("user", user)}
+
+      {user && (
+        <>
+          <Row
+            style={{
+              paddingTop: "1em",
+              paddingBottom: "1em",
+              marginLeft: "1vw",
+              marginRight: "1vw",
+            }}
+          >
+            <Col xs={12} sm={12} md={{ span: 5, offset: 6 }}>
+              <h1 className="login-register-welcome">Welcome {user.name}</h1>
+            </Col>
+          </Row>
+          <Row style={{ marginLeft: "1vw", marginRight: "1vw" }}>
+            <Col xs={12} sm={12} md={{ span: 5, offset: 6 }}>
+              <Button
+                style={{
+                  marginRight: "1em",
+                  padding: "1em 3em",
+                  background: "#090804",
+                  border: "none",
+                  fontWeight: "bold",
+                }}
+              >
+                Go to Dashboard
+              </Button>
+            </Col>
+          </Row>
+        </>
+      )}
     </div>
   );
 };
