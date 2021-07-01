c09a4c39dac22de227b85a1e275f79c8a4fec32e
Max Manayan <maximillian521@gmail.com>
2021-07-01 17:03:11 -0500

    yt-iframe responsive

--- client/src/components/YoutubeEmbed.js
+++ client/src/components/YoutubeEmbed.js
@@ -9,25 +9,24 @@ const YoutubeEmbed = () => {
       <div>
         <YoutubeSelection embedID={embedID} setEmbedID={setEmbedID} />
       </div>
-      {embedID && (
-        <iframe
-          width="853"
-          height="480"
-          // width="425"
-          // height="240"
-          src={`https://www.youtube.com/embed/${embedID}`}
-          frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-          title="Embedded youtube"
-        />
-      )}
-      {!embedID && (
-        <div className="yt-embed-empty-state">
-          <h1>Play Some Background Music!</h1>
-          <h3>Select a Genre Above</h3>
-        </div>
-      )}
+      <div className="yt-iframe-container">
+        {embedID && (
+          <iframe
+            className="yt-iframe"
+            src={`https://www.youtube.com/embed/${embedID}`}
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+            title="Embedded youtube"
+          />
+        )}
+        {!embedID && (
+          <div className="yt-embed-empty-state">
+            <h1>Play Some Background Music!</h1>
+            <h3>Select a Genre Above</h3>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
