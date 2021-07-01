afcf1121e4636590c981786a5ec28a0f1a564b37
Max Manayan <maximillian521@gmail.com>
2021-07-01 12:06:12 -0500

    rainfall embed updated

--- client/src/components/YoutubeSelection.js
+++ client/src/components/YoutubeSelection.js
@@ -42,7 +42,7 @@ const YoutubeSelection = (props) => {
           className={`${
             embedID === "VWPRIe4ft10" ? "yt-genre-active" : "yt-genre"
           }`}
-          onClick={() => setEmbedID("VWPRIe4ft10")}
+          onClick={() => setEmbedID("CFmqh329yW4")}
         >
           Rainfall
         </h5>
