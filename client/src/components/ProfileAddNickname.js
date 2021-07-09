9f9fbb43da93bba17e522b1f65567fa2d9d914f1
Max Manayan <maximillian521@gmail.com>
2021-07-09 15:59:03 -0600

    profile nickname api call setup

--- client/src/components/ProfileAddNickname.js
+++ client/src/components/ProfileAddNickname.js
@@ -1,24 +1,45 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 import { Button, Form } from "react-bootstrap";
 
 const ProfileAddNickname = (props) => {
   const { user } = props;
+  const [nickname, setNickname] = useState(null);
+
+  const addNickname = async (e) => {
+    e.preventDefault();
+    console.log(nickname);
+    try {
+      let res = await axios.put("/api/edit_user", {
+        ...user,
+        nickname: nickname,
+      });
+      console.log("user", user);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <>
       {!user.nickname && (
         <div className="user-profile-nickname-form">
-          <h3>Add Nickname!</h3>
-          <Form>
+          <h3>Nickname:</h3>
+          <Form onSubmit={addNickname}>
             <Form.Group>
-              <Form.Control />
+              <Form.Control
+                placeholder="Add Nickname"
+                id="nickname"
+                onChange={(e) => setNickname(e.target.value)}
+              />
             </Form.Group>
-            <Button>Add</Button>
+            <Button type="submit">Add</Button>
           </Form>
         </div>
       )}
       {user.nickname && (
         <div>
-          <h3>Nickname: {user.name}</h3>
+          <h3>Nickname: {user.nickname}</h3>
         </div>
       )}
     </>
