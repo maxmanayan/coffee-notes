592ae4ee265ba5af2a343b908d20bd271a09141f
Max Manayan <maximillian521@gmail.com>
2021-07-09 15:46:01 -0600

    profile nickname form setup

--- client/src/components/ProfileAddNickname.js
+++ client/src/components/ProfileAddNickname.js
@@ -1,12 +1,24 @@
 import React from "react";
+import { Button, Form } from "react-bootstrap";
 
 const ProfileAddNickname = (props) => {
   const { user } = props;
   return (
     <>
       {!user.nickname && (
-        <div>
+        <div className="user-profile-nickname-form">
           <h3>Add Nickname!</h3>
+          <Form>
+            <Form.Group>
+              <Form.Control />
+            </Form.Group>
+            <Button>Add</Button>
+          </Form>
+        </div>
+      )}
+      {user.nickname && (
+        <div>
+          <h3>Nickname: {user.name}</h3>
         </div>
       )}
     </>
