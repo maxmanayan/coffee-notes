4774d52df52667d4d731183938a576bf39673df7
Max Manayan <maximillian521@gmail.com>
2021-07-23 15:09:40 -0600

    flashcards controller setup

--- app/controllers/api/flashcards_controller.rb
+++ app/controllers/api/flashcards_controller.rb
@@ -1,2 +1,45 @@
 class Api::FlashcardsController < ApplicationController
+
+  def index
+    render json: @deck.flashcards.all
+  end
+
+  def show
+    render json: @deck.flashcards.find(params[:id])
+  end
+
+  def create
+    new_flashcard = @deck.flashcards.new(flashcard_params)
+
+    if (new_flashcard.save)
+      render json: new_flashcard
+    else
+      render json: {errors: new_flashcard.errors}
+    end
+  end
+
+  def update
+    flashcard = @deck.flashcards.find(params[:id])
+
+    if (flashcard.update(flashcard_params))
+      render json: flashcard
+    else
+      render json: {errors: flashcard.errors}
+    end
+  end
+
+  def delete
+    render json: @deck.flashcard.find(params[:id]).destroy
+  end
+
+
+  private
+
+  def get_deck
+    @deck = Deck.find(params[:deck_id])
+  end
+
+  def flashcard_params
+    params.require(:flashcard).permit(:front, :back, :starred)
+  end
 end
