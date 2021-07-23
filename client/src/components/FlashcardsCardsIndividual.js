571709e7e16e5332938feaab96f6d64ecf01aa6e
Max Manayan <maximillian521@gmail.com>
2021-07-23 16:28:15 -0600

    flashcards icons added and formatted

--- client/src/components/FlashcardsCardsIndividual.js
+++ client/src/components/FlashcardsCardsIndividual.js
@@ -8,14 +8,35 @@ const FlashcardsCardsIndividual = (props) => {
   const [showFront, setShowFront] = useState(true);
 
   return (
-    <div className="flashcards-individual-card">
-      <div>
-        {showFront && <h4>{flashcard.front}</h4>}
+    <div className="flashcards-individual-card-block">
+      <div className="flashcards-individual-card">
+        <div className="flashcards-subjects-card-icon-container">
+          <div className="flashcards-subjects-card-icon-container-left">
+            <Icon.X
+              onClick={() => console.log("delete clicked")}
+              className="flashcards-subjects-card-delete"
+              size={20}
+            />
+            <Icon.PencilSquare
+              onClick={() => console.log("edit clicked")}
+              className="flashcards-subjects-card-edit"
+              size={20}
+            />
+          </div>
+          <Icon.Star
+            onClick={() => console.log("star clicked")}
+            size={20}
+            className="flashcards-subjects-card-star"
+          />
+        </div>
+        <div>
+          {showFront && <h4>{flashcard.front}</h4>}
 
-        {!showFront && <h4>{flashcard.back}</h4>}
+          {!showFront && <h4>{flashcard.back}</h4>}
+        </div>
       </div>
-      <div>
-        <Icon.ArrowRepeat onClick={() => setShowFront(!showFront)} />
+      <div className="flashcards-individual-card-flip">
+        <Icon.ArrowRepeat onClick={() => setShowFront(!showFront)} size={30} />
       </div>
     </div>
   );
