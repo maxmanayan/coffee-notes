e0207df237138ac044e952c5c030f775f0d6a345
Max Manayan <maximillian521@gmail.com>
2021-08-04 16:37:49 -0600

    basic world clock functional, still not user customizable

--- client/src/components/WorldClock.js
+++ client/src/components/WorldClock.js
@@ -1,19 +1,10 @@
 import React, { useContext, useState } from "react";
-import { Button } from "react-bootstrap";
 import Clock from "react-live-clock";
-import { SettingsContext } from "../providers/SettingsProvider";
 
 const WorldClock = (props) => {
-  const { worldClock, changeWorldClock } = useContext(SettingsContext);
-  // const [format, setFormat] = useState(worldClock.format)
-
   return (
     <div className="world-clock">
-      <Clock
-        format={worldClock.format}
-        ticking={worldClock.ticking}
-        timezone={null}
-      />
+      <Clock format={"h:mm:ss A"} ticking={true} timezone={null} />
     </div>
   );
 };
